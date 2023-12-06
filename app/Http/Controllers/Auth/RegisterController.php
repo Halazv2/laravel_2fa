779a61131bc62da44e4b55318b38958d8453f9fc
Halazv2 <halaziani2003@gmail.com>
2023-12-06 23:34:41 +0100

    Add Google 2FA secret to user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'google2fa_secret' => $data['google2fa_secret'],
         ]);
     }
 
@@ -98,8 +99,16 @@ class RegisterController extends Controller
             $registration_data['email'],
             $registration_data['google2fa_secret']
         );
-
         // Pass the QR barcode image to our view
         return view('google2fa.register', ['QR_Image' => $QR_Image, 'secret' => $registration_data['google2fa_secret']]);
+    }
+
+    public function completeRegistration(Request $request)
+    {        
+        // add the session data back to the request input
+        $request->merge(session('registration_data'));
+
+        // Call the default laravel authentication
+        return $this->registration($request);
     }
 }
